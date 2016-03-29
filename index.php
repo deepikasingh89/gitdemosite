2f63e35c0d9830613179329f08b44308feaa908e
Deepika singh <singhdeepikad2009@gmail.com>
2016-03-29 14:26:46 +0530

    another change

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
   <input type="text" name="lastname" value="Mouse">
   <br><br>
   <input type="submit" value="Submit">
+   SurName:<br>
+  <input type="text" name="lastname" value="Mouse">
+  <br><br>
+  <input type="submit" value="Submit">
 </form> 
 
 <p>If you click the "Submit" button, the form-data will be sent to a page called "action_page.php".</p>
